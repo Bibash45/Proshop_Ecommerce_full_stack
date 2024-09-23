adb943e18146d47bfabf4d4ed052b7f5bd8300f6
Bibash45 <bibashcdry46@gmail.com>
2024-09-24 00:15:03 +0545

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      app
-    </div>
-  )
-}
+  return <p>Welcome to proshop</p>;
+};
 
-export default App
+export default App;
